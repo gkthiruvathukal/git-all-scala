ea70dd4991fd5784dd2239a3a81fa3fe90365ea5
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-05-07 22:27:50 -0500

    a bit of cleanup

--- git-commits-cloc.scala
+++ git-commits-cloc.scala
@@ -2,7 +2,7 @@
 
 import $ivy.`com.chuusai::shapeless:2.4.0-M1`
 import $ivy.`org.log4s::log4s:1.9.0`
-import $ivy.`org.slf4j::slf4j-simple:1.7.30`
+import $ivy.`org.slf4j:slf4j-simple:1.7.30`
 
 import ammonite.ops._
 import ammonite.ops.ImplicitWd._
@@ -10,24 +10,34 @@ import scala.collection.mutable.ListBuffer
 import scala.io.StdIn.readLine
 
 @main
-def hello(person: String): Unit = {
-    println(f"Hello ${person}")
+def hello(
+       @arg(doc = "person to greet")
+       person: String,
+       @arg(doc = "number of greetings to extend to person (default = 1)")
+       count: Int = 1,
+       @arg(doc = "flag (default = True)")
+       flag : Boolean = true): Unit = {
+
+   val logger = org.log4s.getLogger
+   logger.debug(f"person = ${person}, count = ${count}")
+   (1 to count) map { i => logger.info(f"Hello ${person} ${i}") } 
 }
 
 @main
-def commits(url: String, src: String, dst: String, start: Int, stride: Int): Unit = {
-    val repoURL = url
-    val sourceFolder = src
-    val destinationFolder = dst
-    val startHash = start
+def commits(
+  @arg(doc = "URL of repo (must be able to clone from this URL)") url: String,
+  @arg(doc = "source folder for cloning") src: String,
+  @arg(doc = "dstination folder for exploding commits") dst: String,
+  @arg(doc = "start at commit (e.g. 0 is the first commit in branch)") start: Int,
+  @arg(doc = "skip commmits (e.g. go from 0 to 0 + stride value") stride: Int): Unit = {
 
     val logger = org.log4s.getLogger
 
     logger.info("I'm logging, Baby")
 
     //Establishes path for source folder where clone occurs and destination folder which will recieve every commit
-    val sourcePath = root / sourceFolder
-    val destinationPath = root / destinationFolder
+    val sourcePath = root / src
+    val destinationPath = root / dst
 
     //Checks whether these two folders already exist and if so exits the program and alerts the user
     if (exists ! sourcePath) {
@@ -40,15 +50,15 @@ def commits(url: String, src: String, dst: String, start: Int, stride: Int): Uni
     }
 
     //Clones repo into source folder
-    %.git("clone", repoURL)( root / sourceFolder)
+    %.git("clone", url)( root / src)
 
     //Gets the hashs for each commit and prepares them as an iterator
-    val logForList = hashCodes(sourceFolder).toList
-    val currentNodeHashes = for (i <- (startHash to logForList.size - 1 by stride).toList) yield logForList(i)
+    val logForList = hashCodes(src).toList
+    val currentNodeHashes = for (i <- (start to logForList.size - 1 by stride).toList) yield logForList(i)
         val logIterator = currentNodeHashes.toIterator
 
         //Creates folder where all commits will be placed as subfolders
-        mkdir ! root / destinationFolder
+        mkdir ! root / dst
 
         //This loop creates a new folder for each commit and fills it with the files that were current as of that commit
         for (currentHash <- logIterator) {
